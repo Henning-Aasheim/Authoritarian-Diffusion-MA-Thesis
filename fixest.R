d635e5a3e276f4d37e83786139096a91486bfa29
Henning Aasheim <henniner@gmail.com>
2025-03-26 16:40:44 +0100

    Saving changes to models

--- fixest.R
+++ fixest.R
@@ -37,10 +37,21 @@ fixest_models <- list(
   'Model 6' = fixest_m6
 )
 
+h1_simple_map <- list(
+  'fbic'            = 'Linkages to China',
+  'gdppc_log'       = 'log(GDP per capita)',
+  'rents'           = 'Resource rents',
+  'oda'             = 'Aid',
+  'west_2_fbic'     = 'Linkages (West)',
+  'factor(regime)1' = 'Electoral autocracy',
+  'factor(regime)2' = 'Electoral democracy',
+  'factor(regime)3' = 'Liberal democracy'
+)
+
 modelsummary(fixest_models, 
              stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001), 
-             gof_map = c('nobs', 'vcov.type', 'FE: country', 'FE: year', 'adj.r.squared', 'r2.within.adjusted'),
-             output = 'latex')
+             coef_map = h1_simple_map,
+             gof_map = c('nobs', 'vcov.type', 'FE: country', 'FE: year', 'adj.r.squared', 'r2.within.adjusted'))
 
 # The high R squared is a result of the fixed effects, where I control for many 
 
@@ -60,167 +71,108 @@ fixest_m6_delta <- feols(l(freedom, 1) ~ delta_fbic + gdppc_log + rents + oda + 
 
 
 fixest_models_delta <- list(
-  'm1' = fixest_m1_delta,
-  'm2' = fixest_m2_delta,
-  'm3' = fixest_m3_delta,
-  'm4' = fixest_m4_delta,
-  'm5' = fixest_m5_delta,
-  'm6' = fixest_m6_delta
+  'Model 7' = fixest_m1_delta,
+  'Model 8' = fixest_m2_delta,
+  'Model 9' = fixest_m3_delta,
+  'Model 10' = fixest_m4_delta,
+  'Model 11' = fixest_m5_delta,
+  'Model 12' = fixest_m6_delta
 )
 
-modelsummary(fixest_models_delta, stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001))
-
-# Lag 1 ------------------------------------------------------------------------
-
-fixest_m1_lag_1 <- feols(freedom ~ l(fbic, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m2_lag_1 <- feols(freedom ~ l(fbic, 1) + l(gdppc_log, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m3_lag_1 <- feols(freedom ~ l(fbic, 1) + l(gdppc_log, 1) + l(rents, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m4_lag_1 <- feols(freedom ~ l(fbic, 1) + l(gdppc_log, 1) + l(rents, 1) + l(oda, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m5_lag_1 <- feols(freedom ~ l(fbic, 1) + l(gdppc_log, 1) + l(rents, 1) + l(oda, 1) + l(west_2_fbic, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m6_lag_1 <- feols(freedom ~ l(fbic, 1) + l(gdppc_log, 1) + l(rents, 1) + l(oda, 1) + l(west_2_fbic, 1) + l(factor(regime), 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_models_lag_1 <- list(
-  'm1' = fixest_m1_lag_1,
-  'm2' = fixest_m2_lag_1,
-  'm3' = fixest_m3_lag_1,
-  'm4' = fixest_m4_lag_1,
-  'm5' = fixest_m5_lag_1,
-  'm6' = fixest_m6_lag_1
+h1_delta_map <- list(
+  'delta_fbic'      = 'Linkages to China',
+  'gdppc_log'       = 'log(GDP per capita)',
+  'rents'           = 'Resource rents',
+  'oda'             = 'Aid',
+  'west_2_fbic'     = 'Linkages (West)',
+  'factor(regime)1' = 'Electoral autocracy',
+  'factor(regime)2' = 'Electoral democracy',
+  'factor(regime)3' = 'Liberal democracy'
 )
 
-modelsummary(fixest_models_lag_1, stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001))
+modelsummary(fixest_models_delta, 
+             stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001),
+             coef_map = h1_delta_map,
+             gof_map = c('nobs', 'vcov.type', 'FE: country', 'FE: year', 'adj.r.squared', 'r2.within.adjusted'))
 
-# Lag 1 delta_fbic -------------------------------------------------------------
+# lagged delta_fbic ------------------------------------------------------------
 
-fixest_m1_lag_1_delta <- feols(freedom ~ l(delta_fbic, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
+fixest_m6_delta_2 <- feols(l(freedom, 2) ~ delta_fbic + gdppc_log + rents + oda + west_2_fbic + factor(regime) | country + year, data = base, cluster = 'country', panel.id = ~country+year)
 
-fixest_m2_lag_1_delta <- feols(freedom ~ l(delta_fbic, 1) + l(gdppc_log, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
+fixest_m6_delta_3 <- feols(l(freedom, 3) ~ delta_fbic + gdppc_log + rents + oda + west_2_fbic + factor(regime) | country + year, data = base, cluster = 'country', panel.id = ~country+year)
 
-fixest_m3_lag_1_delta <- feols(freedom ~ l(delta_fbic, 1) + l(gdppc_log, 1) + l(rents, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
+fixest_m6_delta_4 <- feols(l(freedom, 4) ~ delta_fbic + gdppc_log + rents + oda + west_2_fbic + factor(regime) | country + year, data = base, cluster = 'country', panel.id = ~country+year)
 
-fixest_m4_lag_1_delta <- feols(freedom ~ l(delta_fbic, 1) + l(gdppc_log, 1) + l(rents, 1) + l(oda, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
+fixest_m6_delta_5 <- feols(l(freedom, 5) ~ delta_fbic + gdppc_log + rents + oda + west_2_fbic + factor(regime) | country + year, data = base, cluster = 'country', panel.id = ~country+year)
 
-fixest_m5_lag_1_delta <- feols(freedom ~ l(delta_fbic, 1) + l(gdppc_log, 1) + l(rents, 1) + l(oda, 1) + l(west_2_fbic, 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
 
-fixest_m6_lag_1_delta <- feols(freedom ~ l(delta_fbic, 1) + l(gdppc_log, 1) + l(rents, 1) + l(oda, 1) + l(west_2_fbic, 1) + l(factor(regime), 1) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_models_lag_1_delta <- list(
-  'm1' = fixest_m1_lag_1_delta,
-  'm2' = fixest_m2_lag_1_delta,
-  'm3' = fixest_m3_lag_1_delta,
-  'm4' = fixest_m4_lag_1_delta,
-  'm5' = fixest_m5_lag_1_delta,
-  'm6' = fixest_m6_lag_1_delta
+fixest_models_delta <- list(
+  'Model 12' = fixest_m6_delta,
+  'Model 13' = fixest_m6_delta_2,
+  'Model 14' = fixest_m6_delta_3,
+  'Model 15' = fixest_m6_delta_4,
+  'Model 16' = fixest_m6_delta_5
 )
 
-modelsummary(fixest_models_lag_1_delta, stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001))
-
-# Lag 2 ------------------------------------------------------------------------
-
-fixest_m1_lag_2 <- feols(freedom ~ l(fbic, 2) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m2_lag_2 <- feols(freedom ~ l(fbic, 2) + l(gdppc_log, 2) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m3_lag_2 <- feols(freedom ~ l(fbic, 2) + l(gdppc_log, 2) + l(rents, 2) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m4_lag_2 <- feols(freedom ~ l(fbic, 2) + l(gdppc_log, 2) + l(rents, 2) + l(oda, 2) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m5_lag_2 <- feols(freedom ~ l(fbic, 2) + l(gdppc_log, 2) + l(rents, 2) + l(oda, 2) + l(west_2_fbic, 2) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m6_lag_2 <- feols(freedom ~ l(fbic, 2) + l(gdppc_log, 2) + l(rents, 2) + l(oda, 2) + l(west_2_fbic, 2) + l(factor(regime), 2) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_models_lag_2 <- list(
-  'm1' = fixest_m1_lag_2,
-  'm2' = fixest_m2_lag_2,
-  'm3' = fixest_m3_lag_2,
-  'm4' = fixest_m4_lag_2,
-  'm5' = fixest_m5_lag_2,
-  'm6' = fixest_m6_lag_2
+h1_delta_map <- list(
+  'delta_fbic'      = 'Linkages to China',
+  'gdppc_log'       = 'log(GDP per capita)',
+  'rents'           = 'Resource rents',
+  'oda'             = 'Aid',
+  'west_2_fbic'     = 'Linkages (West)',
+  'factor(regime)1' = 'Electoral autocracy',
+  'factor(regime)2' = 'Electoral democracy',
+  'factor(regime)3' = 'Liberal democracy'
 )
 
-modelsummary(fixest_models_lag_2, stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001))
+modelsummary(fixest_models_delta, 
+             stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001),
+             coef_map = h1_delta_map,
+             gof_map = c('nobs', 'vcov.type', 'FE: country', 'FE: year', 'adj.r.squared', 'r2.within.adjusted'))
 
-# Lag 3 ------------------------------------------------------------------------
 
-fixest_m1_lag_3 <- feols(freedom ~ l(fbic, 3) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m2_lag_3 <- feols(freedom ~ l(fbic, 3) + l(gdppc_log, 3) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m3_lag_3 <- feols(freedom ~ l(fbic, 3) + l(gdppc_log, 3) + l(rents, 3) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m4_lag_3 <- feols(freedom ~ l(fbic, 3) + l(gdppc_log, 3) + l(rents, 3) + l(oda, 3) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m5_lag_3 <- feols(freedom ~ l(fbic, 3) + l(gdppc_log, 3) + l(rents, 3) + l(oda, 3) + l(west_2_fbic, 3) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m6_lag_3 <- feols(freedom ~ l(fbic, 3) + l(gdppc_log, 3) + l(rents, 3) + l(oda, 3) + l(west_2_fbic, 3) + l(factor(regime), 3) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_models_lag_3 <- list(
-  'm1' = fixest_m1_lag_3,
-  'm2' = fixest_m2_lag_3,
-  'm3' = fixest_m3_lag_3,
-  'm4' = fixest_m4_lag_3,
-  'm5' = fixest_m5_lag_3,
-  'm6' = fixest_m6_lag_3
-)
-
-modelsummary(fixest_models_lag_3, stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001))
-
-# Lag 4 ------------------------------------------------------------------------
-
-fixest_m1_lag_4 <- feols(freedom ~ l(fbic, 4) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m2_lag_4 <- feols(freedom ~ l(fbic, 4) + l(west_2_fbic, 4) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m3_lag_4 <- feols(freedom ~ l(fbic, 4) + l(west_2_fbic, 4) + l(gdppc_log, 4) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m4_lag_4 <- feols(freedom ~ l(fbic, 4) + l(west_2_fbic, 4) + l(gdppc_log, 4) + l(rents, 4) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m5_lag_4 <- feols(freedom ~ l(fbic, 4) + l(west_2_fbic, 4) + l(gdppc_log, 4) + l(rents, 4) + l(oda, 4) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m6_lag_4 <- feols(freedom ~ l(fbic, 4) + l(west_2_fbic, 4) + l(gdppc_log, 4) + l(rents, 4) + l(oda, 4) + l(regime, 4) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_models_lag_4 <- list(
-  'm1' = fixest_m1_lag_4,
-  'm2' = fixest_m2_lag_4,
-  'm3' = fixest_m3_lag_4,
-  'm4' = fixest_m4_lag_4,
-  'm5' = fixest_m5_lag_4,
-  'm6' = fixest_m6_lag_4
-)
-
-modelsummary(fixest_models_lag_4, stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001))
-
-# Lag 5 ------------------------------------------------------------------------
-
-fixest_m1_lag_5 <- feols(freedom ~ l(fbic, 5) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m2_lag_5 <- feols(freedom ~ l(fbic, 5) + l(west_2_fbic, 5) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m3_lag_5 <- feols(freedom ~ l(fbic, 5) + l(west_2_fbic, 5) + l(gdppc_log, 5) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m4_lag_5 <- feols(freedom ~ l(fbic, 5) + l(west_2_fbic, 5) + l(gdppc_log, 5) + l(rents, 5) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m5_lag_5 <- feols(freedom ~ l(fbic, 5) + l(west_2_fbic, 5) + l(gdppc_log, 5) + l(rents, 5) + l(oda, 5) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_m6_lag_5 <- feols(freedom ~ l(fbic, 5) + l(west_2_fbic, 5) + l(gdppc_log, 5) + l(rents, 5) + l(oda, 5) + l(regime, 5) | iso3c + year, data = base, cluster = 'iso3c', panel.id = ~iso3c+year)
-
-fixest_models_lag_5 <- list(
-  'm1' = fixest_m1_lag_5,
-  'm2' = fixest_m2_lag_5,
-  'm3' = fixest_m3_lag_5,
-  'm4' = fixest_m4_lag_5,
-  'm5' = fixest_m5_lag_5,
-  'm6' = fixest_m6_lag_5
-)
-
-modelsummary(fixest_models_lag_5, stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001))
 
 # Test -------------------------------------------------------------------------
+
+# Simple -----------------------------------------------------------------------
+
+fixest_m1 <- feols(l(freedom, 1) ~ fbic | country + year, data = base, cluster = 'country', panel.id = ~country+year)
+
+fixest_m2 <- feols(l(freedom, 1) ~ fbic + gdppc_log | country + year, data = base, cluster = 'country', panel.id = ~country+year)
+
+fixest_m3 <- feols(l(freedom, 1) ~ fbic + gdppc_log + rents | country + year, data = base, cluster = 'country', panel.id = ~country+year)
+
+fixest_m4 <- feols(l(freedom, 1) ~ fbic + gdppc_log + rents + oda | country + year, data = base, cluster = 'country', panel.id = ~country+year)
+
+fixest_m5 <- feols(l(freedom, 1) ~ fbic + gdppc_log + rents + oda + west_2_fbic | country+ year, data = base, cluster = 'country', panel.id = ~country+year)
+
+fixest_m6 <- feols(l(freedom, 1) ~ fbic + gdppc_log + rents + oda + west_2_fbic + factor(regime) | country + year, data = base, cluster = 'country', panel.id = ~country+year)
+
+
+fixest_models <- list(
+  'Model 1' = fixest_m1,
+  'Model 2' = fixest_m2,
+  'Model 3' = fixest_m3,
+  'Model 4' = fixest_m4,
+  'Model 5' = fixest_m5,
+  'Model 6' = fixest_m6
+)
+
+h1_simple_map <- list(
+  'fbic'            = 'Linkages to China',
+  'gdppc_log'       = 'log(GDP per capita)',
+  'rents'           = 'Resource rents',
+  'oda'             = 'Aid',
+  'west_2_fbic'     = 'Linkages (West)',
+  'factor(regime)1' = 'Electoral autocracy',
+  'factor(regime)2' = 'Electoral democracy',
+  'factor(regime)3' = 'Liberal democracy'
+)
+
+modelsummary(fixest_models, 
+             stars = c("x" = .1, "*" = .05,"**" = .01, '***' = .001), 
+             coef_map = h1_simple_map,
+             gof_map = c('nobs', 'vcov.type', 'FE: country', 'FE: year', 'adj.r.squared', 'r2.within.adjusted'))
 
 ## Lag 3 -----------------------------------------------------------------------
 
